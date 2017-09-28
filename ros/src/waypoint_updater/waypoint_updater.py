3feae1b3109cd392005b5b90524a82e61ce305f9
Josh Smith <racersmith@gmail.com>
2017-09-28 06:00:08 -0700

    Update Waypoints - Velocity subscription - Update waypoint method -- Find closest waypoint -- Find next waypoint -- Construct msg with next waypoints
    TODO:
    - integrate velocity updates using jmt

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -2,7 +2,7 @@
 
 import rospy
 from geometry_msgs.msg import PoseStamped
-from styx_msgs.msg import Lane, Waypoint
+from styx_msgs.msg import Lane, Waypoint, TwistStamped
 
 import math
 
@@ -17,11 +17,18 @@ Once you have created dbw_node, you will update this node to use the status of t
 Please note that our simulator also provides the exact location of traffic lights and their
 current status in `/vehicle/traffic_lights` message. You can use this message to build this node
 as well as to verify your TL classifier.
-
-TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+
+# Acceleration limit to determine if the car can stop for the light
+# This serves as a quick calculation and does not use JMT.
+# Accleration may actually exceed this value by some amount.
+RED_LIGHT_MAX_ACCEL = 9.81
+
+# Target acceleration for velocity changes when situation allows
+# Again this is used for a quick calculation that will feed JMT.
+TARGET_ACCEL = 9.81*0.1
 
 
 class WaypointUpdater(object):
@@ -30,6 +37,7 @@ class WaypointUpdater(object):
 
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
+        rospy.Subscriber('/current_velocity', TwistStamped, self.velocity_cb)
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
         # rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
@@ -38,76 +46,70 @@ class WaypointUpdater(object):
 
         # TODO: Add other member variables you need below
         self.base_waypoints = None
-        self.len_base_waypoints = None
+        self.base_s = [0]
+        self.n_base_waypoints = None
+        self.traffic_index = None
 
-        self.publisher()
+        # Current State
+        self.pose = None
+        self.pos = None
+        self.yaw = None
+        self.vel = None
+
         rospy.spin()
 
-
+    # Callback method for current pose subscription
     def pose_cb(self, msg):
-        pos = msg.pose.pose.position
-        yaw = msg.pose.pose.orientation[2]
-
-        if self.base_waypoints is not None:
-            # TODO: Implement
-            # find closest waypoint
-            closest = self.distance(pos, self.base_waypoints[0].pose.pose.position)
-            closest_index = 0
-            for i in range(1, len(self.base_waypoints)):
-                test_distance = self.distance(pos, self.base_waypoints[i])
-                if test_distance < closest:
-                    closest = test_distance
-                    closest_index = i
-
-            # from heading determine next waypoint
-            closest_point = self.base_waypoints[closest_index].pose.pose.position
-            angle_to_closest = math.atan2(closest_point.y - pos.y, closest_point.x - pos.x)
-            heading = abs(yaw - angle_to_closest)
-            if heading > math.pi/4:
-                closest_index += 1
-
-            # Publish final waypoints
-            final_waypoints = []
-            for i in range(closest_index, closest_index + LOOKAHEAD_WPS):
-                final_waypoints.append(self.base_waypoints[i%self.len_base_waypoints])
-            self.final_waypoints_pub.publish(final_waypoints)
-            pass
+        self.pose = msg
+        self.pos = msg.pose.pose.position
+        self.yaw = msg.pose.pose.orientation[2]
 
 
+    # # Construct final waypoint msg and publish
+    # def pub_waypoints(self, waypoints):
+    #     lane = Lane()
+    #     lane.header.frame_id = '/world'
+    #     lane.header.stamp = rospy.Time.now()
+    #     lane.waypoints = waypoints
+    #     self.final_waypoints_pub.publish(lane)
+
+    # Callback method for velocity subscription
+    def velocity_cb(self, msg):
+        self.vel = msg.twist.linear.x
+
+    # Callback method for waypoint subscription
     def waypoints_cb(self, waypoints):
-        # TODO: Implement
         # base_waypoints is only published once on startup.
         # We need to capture and store this info for later.
-
         self.base_waypoints = waypoints.waypoints
-        self.len_base_waypoints = len(self.base_waypoints)
+        self.n_base_waypoints = len(self.base_waypoints)
+        for i in range(1, self.n_base_waypoints):
+            dist = self.distance(self.base_waypoints[i-1], self.base_waypoints[i])
+            self.base_s.append(self.base_s[-1] + dist)
+
+    # Callback method for traffic light detection subscription
+    def traffic_cb(self, msg):
+        # At what future waypoint index is a red light?
+        self.traffic_index = msg
+
+    # Callback method for obstacle detection subscription
+    def obstacle_cb(self, msg):
+        # TODO: Callback for /obstacle_waypoint message. We will implement it later
         pass
 
-
-    def traffic_cb(self, msg):
-        if self.final_waypoints is not None:
-            # TODO: Callback for /traffic_waypoint message. Implement
-            pass
-
-
-    def obstacle_cb(self, msg):
-        if self.final_waypoints is not None:
-            # TODO: Callback for /obstacle_waypoint message. We will implement it later
-            pass
-
-
+    # Get velocity from single waypoint
     def get_waypoint_velocity(self, waypoint):
         return waypoint.twist.twist.linear.x
 
-
+    # Set velocity of single waypoint
     def set_waypoint_velocity(self, waypoints, waypoint, velocity):
         waypoints[waypoint].twist.twist.linear.x = velocity
 
-
+    # Euclidean distance between two points in 3D space
     def distance(self, a, b):
         return math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2 + (a.z-b.z)**2)
 
-
+    # Cumulative distance between series of waypoints
     def cumulative_distance(self, waypoints, wp1, wp2):
         dist = 0
         for i in range(wp1, wp2+1):
@@ -115,12 +117,47 @@ class WaypointUpdater(object):
             wp1 = i
         return dist
 
-
-    def publisher(self):
+    # Update waypoints and publish
+    def update_waypoints(self):
         rate = rospy.Rate(10)  # 10hz
         while not rospy.is_shutdown():
-            self.final_waypoints_pub.publish(self.final_waypoints)
+            if self.base_waypoints is not None:
+                # TODO: Implement
+                # find closest waypoint
+                closest = self.distance(self.pos, self.base_waypoints[0].pose.pose.position)
+                closest_index = 0
+                for i in range(1, len(self.base_waypoints)):
+                    test_distance = self.distance(self.pos, self.base_waypoints[i])
+                    if test_distance < closest:
+                        closest = test_distance
+                        closest_index = i
+
+                # from heading determine next waypoint
+                closest_point = self.base_waypoints[closest_index].pose.pose.position
+                angle_to_closest = math.atan2(closest_point.y - self.pos.y, closest_point.x - self.pos.x)
+                heading = abs(self.yaw - angle_to_closest)
+                if heading > math.pi / 4:
+                    closest_index += 1
+
+                # Publish final waypoints
+                final_waypoints = []
+                for i in range(closest_index, closest_index + LOOKAHEAD_WPS):
+                    final_waypoints.append(self.base_waypoints[i % self.n_base_waypoints])
+
+                # Construct and publish message
+                lane = Lane()
+                lane.header.frame_id = '/world'
+                lane.header.stamp = rospy.Time.now()
+                lane.waypoints = final_waypoints
+                self.final_waypoints_pub.publish(lane)
             rate.sleep()
+
+    # # Publish
+    # def publisher(self):
+    #     rate = rospy.Rate(10)  # 10hz
+    #     while not rospy.is_shutdown():
+    #         self.final_waypoints_pub.publish(self.final_waypoints)
+    #         rate.sleep()
 
 
 if __name__ == '__main__':
