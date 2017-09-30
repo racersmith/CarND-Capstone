d7e98d6a4534001473379783af64362b18a2d271
Josh Smith <racersmith@gmail.com>
2017-09-30 12:12:53 -0700

    twist_controller first pass - determine max braking torque - intitialize controllers - Deal with step time in class - implement controllers - return throttle, brake and steering commands

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,14 +1,54 @@
+import rospy
+from pid import PID
+from yaw_controller import YawController
+
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
-        # TODO: Implement
-        pass
+    def __init__(self,
+                 vehicle_mass,
+                 fuel_capacity,
+                 brake_deadband,
+                 decel_limit,
+                 accel_limit,
+                 wheel_radius,
+                 wheel_base,
+                 steer_ratio,
+                 max_lat_accel,
+                 max_steer_angle):
 
-    def control(self, *args, **kwargs):
-        # TODO: Change the arg, kwarg list to suit your needs
+        # Maximum braking torque
+        # F = m*a
+        # T = F*r
+        # T = m*a*r
+        maximum_braking_torque = (vehicle_mass+fuel_capacity*GAS_DENSITY)*decel_limit*wheel_radius
+
+        # Initialize PID controllers for throttle, brake and steering
+        self.throttle_controller = PID(kp=0.1, kd=0.0, ki=0.0, mn=0, mx=1)
+        self.brake_controller = PID(kp=0.1, kd=0.0, ki=0.0, mn=brake_deadband, mx=maximum_braking_torque)
+        self.steering_controller = YawController(wheel_base, steer_ratio, 0.01, max_lat_accel, max_steer_angle)
+
+        # time_step value
+        self.last_time = ros.get_time()
+
+
+    # Determine control commands from target velocities
+    def control(self, linear_target, linear_current, angular_target, angular_current, dbw_enabled):
+        # if dbw is not enabled don't let PID controllers windup.
+        if not dbw_enabled:
+            self.throttle_controller.reset()
+            self.brake_controller.reset()
+
+        linear_error = linear_target - linear_current
+        current_time = ros.get_time()
+        step_time = current_time - self.last_time
+        self.last_time = current_time
+        throttle_cmd = self.throttle_controller.step(linear_error, step_time)
+        brake_cmd = self.brake_controller.step(-linear_error, step_time)
+        steering_cmd = self.steering_controller.get_steering(linear_current, angular_target, angular_current)
+
         # Return throttle, brake, steer
-        return 1., 0., 0.
+        return throttle_cmd, brake_cmd, steering_cmd
