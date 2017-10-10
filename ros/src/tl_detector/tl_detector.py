92fa5449beb5f416ceefbb49e2bee129c17f28af
Josh Smith <racersmith@gmail.com>
2017-10-10 06:31:02 -0700

    store just waypoints without header

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -66,7 +66,7 @@ class TLDetector(object):
     def waypoints_cb(self, waypoints):
         if self.waypoints is None:
             # self.waypoints = [pose.pose.postion for pose in waypoints.pose]
-            self.waypoints = waypoints
+            self.waypoints = waypoints.waypoints
             # stop_lines = [ for x, y in self.config['stop_line_positions']]
             # for i, point in enumerate(stop_lines):
             #     closest_index = self.get_closest_waypoint(point)
