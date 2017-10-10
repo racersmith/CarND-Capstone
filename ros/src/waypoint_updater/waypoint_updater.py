57f455f932a6aeec4f077a5a9ef820e94698deec
Josh Smith <racersmith@gmail.com>
2017-10-10 06:20:58 -0700

    Int32 import from ros msgs

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -4,6 +4,7 @@ import rospy
 import tf
 from geometry_msgs.msg import PoseStamped, Quaternion, TwistStamped
 from styx_msgs.msg import Lane, Waypoint
+from std_msgs.msg import Int32
 
 
 import math
